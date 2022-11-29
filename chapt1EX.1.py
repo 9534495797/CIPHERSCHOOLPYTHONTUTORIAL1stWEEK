519b137de1669e943b1f2dd0326c6e34bd2a39ba
9534495797 <chaubeyankit927@gmail.com>
2022-11-29 18:58:34 +0530

    ok

---
+++ chapt1EX.1.py
@@ -0,0 +1,11 @@
+# print the following 
+
+# this is \\ double backslash 
+# these are /\/\/\/\/\ mountains 
+# he is  awesome 
+# \" \n \t \' 
+
+print("this is \\\\ double backslash ")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("he is \tawesome")
+print("\\\" \\n \\t \\'")
