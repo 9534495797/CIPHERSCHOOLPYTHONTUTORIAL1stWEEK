fa075a1537819dff7c6811ecb1ceb731fc85780f
9534495797 <chaubeyankit927@gmail.com>
2022-11-29 19:16:23 +0530

    ok

---
+++ escape_sequencees.py
@@ -0,0 +1,17 @@
+# \'   single Cote  
+# \"   double cote   
+# \\  back slash  
+# \n new line 
+# \t tab 
+# \b back space  
+
+print("hello\'ankit\' world")
+print("line a \n line b")
+print("line a \nline b \nline c")
+print("name \tANKIT CHAUBEY")
+
+print("this is double backslash \\\\")
+print("hello\blo")
+
+
+print("line a 0\\n line b")
